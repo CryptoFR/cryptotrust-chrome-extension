f6ed81e0d47a9491b7e5dfd1fbfaa79e00bdc391
Romain Lafforgue <slashbinslashnoname@gmail.com>
2018-05-06 04:45:10 +0200

    Initial commit

---
+++ js/mallory.js
@@ -0,0 +1,66 @@
+
+var Mallory = {
+    instance:null,
+    url: "https://raw.githubusercontent.com/CryptoFR/crypto-scams-fr/master/websites.txt",
+    ttl: 5,
+    debugging : true,
+
+    init : function(){
+
+      Mallory.debug("Scanning scammy sites.");
+      Mallory.verifyFreshness();
+      Mallory.isMallory();
+    },
+    verifyFreshness : function(){
+      // Verify Storage and freshness
+      chrome.storage.local.get(['lastDownload'], function(result) {
+        Mallory.debug("Timestamp " + result.lastDownload);
+        if(result.lastDownload === undefined || result.lastDownload + Mallory.ttl < Number.parseInt(Date.now()/1000, 0)) {
+          Mallory.getDistantDatabase();
+        }
+      });
+
+    },
+    getDistantDatabase: function(){
+      // FIXME : ugly pooling
+      var xmlhttp;
+      xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function(){
+          if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+              chrome.storage.local.set({"lastDownload":  Mallory.timestamp()});
+              chrome.storage.local.set({"scamSites": xmlhttp.responseText});
+          }
+      }
+      xmlhttp.open("GET", Mallory.url, true);
+      xmlhttp.send();
+    },
+    isMallory: function(){
+      return chrome.storage.local.get(['scamSites'], function(result){
+        if(result.scamSites.indexOf(document.domain) != -1) {
+          console.log('🚀');
+          console.log('🤔 Scammy site.');
+          Mallory.showPopup();
+        }
+      });
+
+    },
+    showPopup: function(){
+      window.onload=function(){
+        var save = document.body.innerHTML;
+        fetch(chrome.extension.getURL('scam.html'))
+        .then(response => response.text())
+        .then(data => {
+            document.body.innerHTML = data+document.body.innerHTML;
+        });
+      };
+    },
+    timestamp: function(){
+      return Number.parseInt(Date.now()/1000, 0);
+    },
+    debug: function(message) {
+      if(Mallory.debugging === true) {
+        console.log(message);
+      }
+    }
+};
+Mallory.init();
